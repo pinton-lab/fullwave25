2445ba10302851891ca5353f197aad503c4cbf6a
Masashi Sode <39261814+MasashiSode@users.noreply.github.com>
2025-12-19 12:25:33 -0500

    Bump version: 1.0.19 → 1.0.20-dev0

--- fullwave/__init__.py
+++ fullwave/__init__.py
@@ -60,7 +60,7 @@ try:
     __version__ = version("fullwave")
 except PackageNotFoundError:
     # Update via bump-my-version, not manually
-    __version__ = "1.0.19"
+    __version__ = "1.0.20-dev0"
 
 VERSION = __version__  # for convenience
 logger.info("Fullwave version: %s", __version__)
