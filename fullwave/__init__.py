7e1233c6a6652b4e5e34d68403a38062c6d42d7b
Masashi Sode <39261814+MasashiSode@users.noreply.github.com>
2026-09-22 00:33:39 -0400

    Bump version: 1.3.2-dev6 → 1.3.2-rc0

--- fullwave/__init__.py
+++ fullwave/__init__.py
@@ -66,7 +66,7 @@ try:
     __version__ = version("fullwave")
 except PackageNotFoundError:
     # Update via bump-my-version, not manually
-    __version__ = "1.3.2-dev6"
+    __version__ = "1.3.2-rc0"
 
 VERSION = __version__  # for convenience
 logger.info("Fullwave version: %s", __version__)
